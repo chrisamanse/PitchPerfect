9b14300f6cbc093bfa621bd2f57529af030df6b9
Chris Amanse <chris@chrisamanse.xyz>
2016-10-13 00:11:55 -0700

    Create PlayViewController.swift

---
+++ PitchPerfect/PlayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlayViewController.swift
+//  PitchPerfect
+//
+//  Created by Chris Amanse on 10/13/2016.
+//  Copyright © 2016 Chris Amanse. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
