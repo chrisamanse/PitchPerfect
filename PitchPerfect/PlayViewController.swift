c17dd0abe552407b4ef14d7e1f338b3a5696b85b
Chris Amanse <chris@chrisamanse.xyz>
2016-10-13 01:01:13 -0700

    Connect IBOutlets and IBActions

--- PitchPerfect/PlayViewController.swift
+++ PitchPerfect/PlayViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class PlayViewController: UIViewController {
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     var recordedAudioURL: URL?
     
     override func viewDidLoad() {
@@ -18,5 +26,13 @@ class PlayViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
     }
 }
