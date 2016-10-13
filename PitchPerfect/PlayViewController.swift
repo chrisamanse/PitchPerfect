3b7a17c7dbaded60f5574d1d47bb5773d2fbc2ce
Chris Amanse <chris@chrisamanse.xyz>
2016-10-13 01:42:55 -0700

    Implement audio playback with transforms

--- PitchPerfect/PlayViewController.swift
+++ PitchPerfect/PlayViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayViewController: UIViewController {
     
@@ -18,10 +19,20 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
-    var recordedAudioURL: URL?
+    var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(forPlayState: .notPlaying)
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,9 +41,44 @@ class PlayViewController: UIViewController {
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
         print("Play Sound Button Pressed")
+        
+        guard let buttonType = ButtonType(rawValue: sender.tag) else {
+            print("Unrecognized button")
+            return
+        }
+        
+        switch buttonType {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(forPlayState: .playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
         print("Stop Audio Button Pressed")
+        
+        stopAudio()
+    }
+}
+
+extension PlayViewController {
+    enum ButtonType: Int {
+        case slow = 0
+        case fast
+        case chipmunk
+        case vader
+        case echo
+        case reverb
     }
 }
